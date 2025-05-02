379b83eaf815b4297b0d3a29c6851c5add0211f3
Sander Ronde <awsdfgvhbjn@gmail.com>
2025-05-02 18:54:29 +0200

    add spinning rainbow effect

--- leds/effects/__init__.py
+++ leds/effects/__init__.py
@@ -3,9 +3,11 @@ from leds.effects.rainbow_radial import RainbowRadialEffect
 from leds.effects.single_color_radial import SingleColorRadialEffect
 from leds.effects.multi_color_radial import MultiColorRadialEffect
 from leds.effects.rainbow import RainbowEffect
+from leds.effects.rainbow_spin import RainbowSpinEffect
 from leds.controllers.controller_base import ControllerBase
 from leds.effects.effect import Effect
 from leds.controllers.scale_panel_controller import ScalePanelLEDController
+from leds.controllers.hex_controller import HexPanelLEDController
 
 
 def get_effects(controller: ControllerBase) -> Dict[str, Effect]:
@@ -16,4 +18,6 @@ def get_effects(controller: ControllerBase) -> Dict[str, Effect]:
     }
     if isinstance(controller, ScalePanelLEDController):
         effects[RainbowEffect.__name__] = RainbowEffect(controller)
+    if isinstance(controller, HexPanelLEDController):
+        effects[RainbowSpinEffect.__name__] = RainbowSpinEffect(controller)
     return effects
---
+++ leds/effects/rainbow_spin.py
@@ -0,0 +1,24 @@
+from leds.effects.effect import Effect, SpeedParameters
+from leds.controllers.hex_controller import HexPanelLEDController
+
+
+class RainbowSpinParameters(SpeedParameters):
+    pass
+
+
+class RainbowSpinEffect(Effect):
+    def __init__(self, controller: HexPanelLEDController):
+        super().__init__(controller)
+        self.controller = controller
+        self.PARAMETERS = RainbowSpinParameters()
+
+    def run(self, ms: int):
+        offset = Effect.time_offset(
+            ms, self.PARAMETERS.speed.get_value(), self.PARAMETERS.direction.get_value())
+
+        for panel in self.controller.panels:
+            for led_index in panel.panel_config.ordered_leds:
+                angle = panel.get_angle_at_index(led_index)
+                panel.strip.setPixelColor(
+                    led_index, Effect.rainbow((angle / 360) + offset))
+        self.controller.show()
