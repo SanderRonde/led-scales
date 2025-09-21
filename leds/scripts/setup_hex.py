c563e6e5e72b002a05c08e5985cb4aa512d37ef1
Sander Ronde <awsdfgvhbjn@gmail.com>
2025-09-22 01:02:53 +0200

    Add setup script

---
+++ leds/scripts/setup_hex.py
@@ -0,0 +1,59 @@
+from config import HexConfig
+from leds.color import RGBW
+from typing import List, Dict
+from leds.controllers.controller_base import get_library
+import time
+
+
+def main():
+    config = HexConfig()
+    PixelStrip, _ = get_library(True)
+    (pin, channel) = config.pins
+    
+    strip = PixelStrip(
+        num=10000,
+        pin=pin,
+        brightness=255,
+        freq_hz=800000,
+        dma=10,
+        channel=channel,
+    )
+    strip.begin()
+
+    led_indices: Dict[int, List[int]] = {}
+    current_led = 0
+
+    try:
+        while True:
+            # Turn off all LEDs
+            for i in range(10000):
+                strip.setPixelColor(i, RGBW(0, 0, 0, 0))
+
+            # Blink the current LED
+            strip.setPixelColor(current_led, RGBW(255, 255, 255, 0))
+            strip.show()
+            time.sleep(0.1)
+            strip.setPixelColor(current_led, RGBW(0, 0, 0, 0))
+            strip.show()
+            time.sleep(0.1)
+
+            # Get user input
+            user_input = input(f"Hex for pixel {current_led}: ")
+            if user_input.isdigit():
+                user_num = int(user_input)
+                if user_num not in led_indices:
+                    led_indices[user_num] = []
+                led_indices[user_num].append(current_led)
+                current_led += 1
+
+    except KeyboardInterrupt:
+        # Dump the array in the format of HexConfig
+        result = "self.hexagons = [\n"
+        for index, hexagon in enumerate(config.hexagons):
+            result += f"\tHexagon({hexagon.x}, {hexagon.y}, [{', '.join(map(str, led_indices.get(index, [])))}]),\n"
+        result += "]"
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
